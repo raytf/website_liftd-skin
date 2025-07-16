92cad2db0aced03cdae3543fb400574758131e60
Ray Feng <ray@dotdothorse.com>
2025-07-16 22:43:44 +1000

    Initial commit from Sanity CLI

---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,39 @@
+import { PageBuilder } from "@/components/pagebuilder";
+import { sanityFetch } from "@/lib/sanity/live";
+import { queryHomePageData } from "@/lib/sanity/query";
+import { getSEOMetadata } from "@/lib/seo";
+
+async function fetchHomePageData(stega = true) {
+  return await sanityFetch({
+    query: queryHomePageData,
+    stega,
+  });
+}
+
+export async function generateMetadata() {
+  const { data: homePageData } = await fetchHomePageData(false);
+  return getSEOMetadata(
+    homePageData
+      ? {
+          title: homePageData?.title ?? homePageData?.seoTitle ?? "",
+          description:
+            homePageData?.description ?? homePageData?.seoDescription ?? "",
+          slug: homePageData?.slug,
+          contentId: homePageData?._id,
+          contentType: homePageData?._type,
+        }
+      : {},
+  );
+}
+
+export default async function Page() {
+  const { data: homePageData } = await fetchHomePageData();
+
+  if (!homePageData) {
+    return <div>No home page data</div>;
+  }
+
+  const { _id, _type, pageBuilder } = homePageData ?? {};
+
+  return <PageBuilder pageBuilder={pageBuilder ?? []} id={_id} type={_type} />;
+}
